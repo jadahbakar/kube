07fabdcbf365cd3856be6fe0462d75fb709de668
jadahbakar <dedy.styawan@gmail.com>
2020-04-30 16:31:51 +0700

    testing

--- pg2/run_partial.sh
+++ pg2/run_partial.sh
@@ -1,9 +1,10 @@
-kubectl create -f postgres-configmap.yaml
-kubectl create -f postgres-service.yaml
 kubectl create -f postgres-storage.yaml
+kubectl create -f postgres-configmap.yaml
 kubectl create -f postgres-deployment.yaml
+kubectl create -f postgres-service.yaml
 kubectl get svc postgres
 # untuk running
 kubectl get pods
 kubectl get namespaces
 # kubectl -n default exec -it postgres-7ffd788bc9-vcdjx -- psql --host 127.0.0.1 --port 5432 --username postgresadmin -W -d postgresdb
+# psql -h localhost -U postgresadmin --password -p 32014 postgresdb
